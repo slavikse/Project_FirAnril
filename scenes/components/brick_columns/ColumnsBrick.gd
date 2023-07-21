52c1f5b83e16dcfa9f0299876a7bf84cba24854b
vyacheslav.lebedev <vyacheslav.lebedev@red-soft.ru>
2023-07-22 00:45:38 +0300

    feat: пушка и снаряд + правки

--- scenes/components/brick_columns/ColumnsBrick.gd
+++ scenes/components/brick_columns/ColumnsBrick.gd
@@ -1,8 +1,8 @@
 extends Area3D
 
-const _COLUMN_HEIGHT := -19.95
-const _COLUMN_HEIGHT_MAX := 2
-const _COLUMN_SPEED_UP := 1.0
+const _COLUMN_HEIGHT := -17.95
+const _COLUMN_HEIGHT_MAX := 2.0
+const _COLUMN_SPEED_UP := 3.0 # 1.0
 const _COLUMN_SPEED_DOWN := _COLUMN_SPEED_UP * 3.0
 
 var _is_lifting := false
