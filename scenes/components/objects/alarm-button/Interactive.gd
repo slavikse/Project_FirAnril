f308f6cc06a6692bca1f17d65600564512934427
vyacheslav.lebedev <vyacheslav.lebedev@red-soft.ru>
2023-10-16 22:52:50 +0300

    up

--- scenes/components/objects/alarm-button/Interactive.gd
+++ scenes/components/objects/alarm-button/Interactive.gd
@@ -1,17 +1,28 @@
 extends Area3D
 
+signal interact(is_interact: bool)
+
 # todo интернациализация. сюда должно быть передано, какую реакцию вызовет и на какую кнопку
 @export var message := ''
 @export var button := ''
+
+var _is_interact := false
+
+func _process(_delta: float) -> void:
+    if _is_interact and Input.is_action_just_pressed('interact'):
+        print('interact - кнопка нажата')
 
 # TODO добавить обработку нажатия клавиши. если в момент, когда приблизился к кнопке нажал Е,
 # то сработает нажатие красной кнопки.
 # еще уведомление на экране, что элемент интерактивный и можно нажать клавишу Е
 
-# interface
 func start_interact() -> void:
-    print('start_interact', message)
+    _is_interact = true
+    interact.emit(_is_interact)
+    # todo показывать уведомление (UI)
+    print('start_interact ', message, ' ', button)
 
-# interface
 func end_interact() -> void:
-    print('end_interact')
+    _is_interact = false
+    interact.emit(_is_interact)
+    # todo скрывать уведомление (UI)
